63d9ac8e649341bdb1761d38c9ceb1e2870affb4
Alexclouth <alazeralphilo@example.com>
2025-11-11 00:21:21 +0300

    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.excellencehometutor.com',
+  siteUrl: "https://www.excellencehometutor.com",
   generateRobotsTxt: true,
-}
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
+};
